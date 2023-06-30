9f3503432cae833f36abc54f1226f509d18c713a
cfoster5 <cfoster3204@gmail.com>
2023-06-29 21:30:32 -0500

    feat: Migrate to app.config.js and add env variables

---
+++ app.config.js
@@ -0,0 +1,43 @@
+export default {
+  expo: {
+    name: "lookforward-expo",
+    slug: "lookforward-expo",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./src/assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./src/assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./src/assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./src/assets/images/favicon.png",
+    },
+    plugins: ["expo-router"],
+    experiments: {
+      tsconfigPaths: true,
+      typedRoutes: true,
+    },
+    extra: {
+      eas: {
+        projectId: "8df76e83-2eba-4eac-a47b-4332ac669597",
+      },
+      TMDB_KEY: process.env.TMDB_KEY || null,
+      IGDB_AWS_KEY: process.env.IGDB_AWS_KEY || null,
+    },
+  },
+};
