90ac7ef2ea1b0eba885059263fa2cb3c80d8ed67
cfoster5 <cfoster3204@gmail.com>
2023-07-07 17:27:53 -0500

    feat: Add SearchBar for movies

--- src/app/(tabs)/(find)/index.tsx
+++ src/app/(tabs)/(find)/index.tsx
@@ -1,5 +1,8 @@
+import { SearchBar } from "@rneui/themed";
 import { useState } from "react";
-import { ActivityIndicator, FlatList } from "react-native";
+import { ActivityIndicator, FlatList, PlatformColor } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { human } from "react-native-typography";
 
 import { MoviePoster } from "@/components/Posters";
 import { View } from "@/components/Themed";
@@ -17,17 +20,40 @@ export default function TabOneScreen() {
 
   const results = data?.pages.flatMap((page) => page.results);
   return (
+    <>
+      <SafeAreaView edges={["top"]}>
+        <SearchBar
+          containerStyle={{
+            backgroundColor: "transparent",
+            paddingHorizontal: 8,
+          }}
+          inputContainerStyle={{
+            backgroundColor: PlatformColor("tertiarySystemFill"),
+            borderRadius: 10,
+          }}
+          inputStyle={[human.body, { color: PlatformColor("label") }]}
+          onChangeText={setSearchValue}
+          placeholder="Movies & People"
+          placeholderTextColor={PlatformColor("secondaryLabel")}
+          platform="ios"
+          searchIcon={{ color: PlatformColor("secondaryLabel") }}
+          value={searchValue}
+        />
+      </SafeAreaView>
+
     <FlatList
+        // contentInsetAdjustmentBehavior="automatic"
       data={results}
       renderItem={({ item }) => <MoviePoster item={item} />}
       ItemSeparatorComponent={() => <View style={{ marginVertical: 8 }} />}
       ListEmptyComponent={() => <ActivityIndicator />}
       columnWrapperStyle={{ columnGap: 16 }}
-      contentContainerStyle={{ padding: 16 }}
+        contentContainerStyle={{ padding: 16, paddingTop: 0 }}
       keyExtractor={(item) => item?.id?.toString()}
       numColumns={2}
       onEndReached={() => (hasNextPage ? fetchNextPage() : null)}
       onEndReachedThreshold={1.5}
     />
+    </>
   );
 }
